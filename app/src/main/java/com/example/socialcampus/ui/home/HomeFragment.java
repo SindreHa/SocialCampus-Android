52a85acc557a269a6a55e9f5aaeed2428856d967
Sindre Haavaldsen <sindre.h@outlook.com>
2019-09-07 00:12:20 +0200

    Laget eksempel på post-kort for grupper

--- app/src/main/java/com/example/socialcampus/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/socialcampus/ui/home/HomeFragment.java
@@ -1,12 +1,16 @@
 package com.example.socialcampus.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -16,18 +20,42 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        /*final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
+
+
+
         return root;
+    }
+
+    private void displayToast(String t) {
+        Toast.makeText(getContext(), t, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        final CardView card_view = getView().findViewById(R.id.kort);
+        final String LOG_TAG = HomeFragment.class.getSimpleName();
+
+        card_view.setOnClickListener(new View.OnClickListener() {
+
+            //String getName = ((TextView)card_view.getChildAt(1)).getText().toString();
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "Klikk på kort");
+                displayToast("Klikk på kort");
+            }
+        });
     }
 }
