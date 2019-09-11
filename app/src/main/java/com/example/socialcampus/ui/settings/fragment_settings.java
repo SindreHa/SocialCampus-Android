c7baf5b726aba16914d05e36cf05aaa301b75ea6
Sindre Haavaldsen <sindre.h@outlook.com>
2019-09-11 12:55:00 +0200

    Opprettet instillinger

---
+++ app/src/main/java/com/example/socialcampus/ui/settings/fragment_settings.java
@@ -0,0 +1,21 @@
+package com.example.socialcampus.ui.settings;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.socialcampus.R;
+public class fragment_settings extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+}
