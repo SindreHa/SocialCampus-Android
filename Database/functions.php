52a36326582ff340d64e09c452c8367ceac46286
KK <EpicElementz@gmail.com>
2019-09-19 09:25:42 +0200

    Funksjon som sjekker om brukeren finnes

---
+++ Database/functions.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ Sjekker om brukeren allerede finnes
+**/
+
+function userExists($username){
+	$query = "SELECT username FROM member WHERE username = ?";
+	global $con;
+	if($stmt = $con->prepare($query)){
+		$stmt->bind_param("s",$username);
+		$stmt->execute();
+		$stmt->store_result();
+		$stmt->fetch();
+		if($stmt->num_rows == 1){
+			$stmt->close();
+			return true;
+		}
+		$stmt->close();
+	}
+
+	return false;
+}
+
+
+}
+?>
