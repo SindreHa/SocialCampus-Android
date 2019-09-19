8277a48d3e532bbe4a61d75df4684542ac5da3c4
Kristian <EpicElementz@gmail.com>
2019-09-19 13:56:58 +0200

    Lengre splash

--- app/src/main/java/com/example/socialcampus/ui/home/SplashActivity.java
+++ app/src/main/java/com/example/socialcampus/ui/home/SplashActivity.java
@@ -1,18 +1,15 @@
 package com.example.socialcampus.ui.home;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
 import android.os.Handler;
-import com.example.socialcampus.MainActivity;
 import com.example.socialcampus.R;
 
 public class SplashActivity extends Activity {
-    // Splash screen timer
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 3500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +18,14 @@ public class SplashActivity extends Activity {
 
         new Handler().postDelayed(new Runnable() {
 
-            /*
-             * Showing splash screen with a timer. This will be useful when you
-             * want to show case your app logo / company
-             */
+
 
             @Override
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
+
                 Intent i = new Intent(SplashActivity.this, com.example.socialcampus.MainActivity.class);
                 startActivity(i);
 
-                // close this activity
                 finish();
             }
         }, SPLASH_TIME_OUT);
