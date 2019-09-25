89fbe481e54be214a02bcd35d50db566fc371b52
Sindre Haavaldsen <sindre.h@outlook.com>
2019-09-25 14:51:05 +0200

    Ryddet opp LogIn.java og SignUp.java

--- app/src/main/java/com/example/socialcampus/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/socialcampus/ui/profile/ProfileFragment.java
@@ -4,24 +4,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 import com.example.socialcampus.R;
 
 public class ProfileFragment extends Fragment {
 
-    private ProfileViewModel shareViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        shareViewModel = ViewModelProviders.of(this).get(ProfileViewModel.class);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        View root = inflater.inflate(R.layout.fragment_profile, container, false);
-
-        return root;
+        return view;
     }
 }
